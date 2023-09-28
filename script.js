953d5e7f75d4ea122854c0fe21bc284b43f1910b
W_o <118588002+To-oTu@users.noreply.github.com>
2023-09-28 18:37:14 +0800

    Add files via upload
    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+let _ = document;
+console.log('document:', _.querySelector('div'));
+
+const soundsText = [
+  'CLAP',
+  'HIHAT',
+  'KICK',
+  'OPENHAT',
+  'BOOM',
+  'RIDE',
+  'SNARE',
+  'TOM',
+  'TINK'
+];
